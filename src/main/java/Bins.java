58e21f29c64d3d124f6b3ae428628857e24d7196
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-06 14:15:12 -0500

    what i have done so far

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,22 @@
 
 public class Bins {
+     public int start;
+     public int stop;
+     private int[] bin;
 
+     public Bins(int start, int stop){
+          this.bin = new int[stop-start];
+          this.start = start;
+          this.stop = stop;
+     }
+
+     public Integer getBin(int binNumber){
+         return this.bin[binNumber-this.start];
+     }
+
+
+
+     public void incrementBin(int binNumber){
+         this.bin[binNumber-this.start]++;
+     }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,34 @@
 public class Simulation {
 
+private int Tosses;
+private int NumDice;
+private Bins bin;
 
+   public Simulation(Integer numberOfDies,Integer numberOfTosses){
+       this.NumDice = numberOfDies;
+       this.Tosses = numberOfTosses;
+       }
 
+    public void runSimulation(){
+        Dice dice = new Dice(this.NumDice);
+        this.bin = new Bins(this.NumDice,(this.NumDice*6));
+        for(int x=1;x<=Tosses;x++) {
+            bin.incrementBin(dice.tossandSum());
+        }
+}
+        public void printresults() {
+            //print results from the bin
+            int sum =0;
+            double avg = 0.0;
+            int diceCount = 0;
+            System.out.println("***\n");
+            System.out.println(" Simulation of "+this.NumDice+" dice tossed for "+this.Tosses+" times.\n");
+            System.out.println("***\n");
+            for (int x = 0; x < bin.stop; x++) {
+                diceCount = x+this.NumDice;
+                sum = this.bin.getBin(x);
+                avg = sum/this.Tosses;
+                System.out.println(diceCount+" :%10d"+sum+" :"+ " %.2f"+avg);
+            }
+        }
 }
